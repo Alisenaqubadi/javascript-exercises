1e5ffaab2782ada846da26cee3fd0bc41dddfc96
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-02-12 19:36:34 +0500

    puzzle solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let totalString = '';
+    let i = 0;
+    let error;
+    if( num < 0 ){
+        return error  = "ERROR";
+        
+    };
 
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+    while (i < num) {
+      totalString += string;
+      i++;
+    }
+    
+    return totalString;
+  };
+  
+  // Do not edit below this line
+  module.exports = repeatString;
+  
+  console.log(repeatString('ali', 0));
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function( str ) {
+    const splitedStr = str.split("");
+    const reversedArray = splitedStr.reverse();
+    const reversedStr = reversedArray.join("");
+    return reversedStr;
+}
 
-};
+console.log(reverseString('ali'));
 
 // Do not edit below this line
 module.exports = reverseString;
