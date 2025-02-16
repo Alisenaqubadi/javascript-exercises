9faf40f65423e5c938702b199ff9a99a5324648d
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-02-16 19:00:38 +0500

    solved the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(starter,ender) {
+    if( !Number.isInteger(starter) || !Number.isInteger(ender)) return "ERROR";
+    if( starter < 0 || ender < 0 ) return "ERROR";
+    if( starter > ender){
+        temp = starter
+        starter = ender
+        ender = temp
+    }
+    let sum = 0;
+    for(let i = starter ; i <= ender ; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll("ALI" ,1));
